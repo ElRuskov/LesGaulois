bd8955fb716df95fbbc0f24718b28e475a777552
Joan <joan.belusca@gmail.com>
2026-01-22 11:48:46 +0100

    TP1 debut du code

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,10 +2,27 @@ package personnages;
 
 public class Gaulois {
 	
+	private String nom;
+	private Integer force;
+	
+	Gaulois(String nom, Integer force){
+		this.nom=nom;
+		this.force=force;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
 	
 	
 	
+	public String parler(String text) {
+		return "Le gaulois "+this.nom+": <<"+text+">>";
+	}
 	
+
+
 	public static void main(String[] args) {
 		
 	}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,23 @@
 package personnages;
 
 public class Romain {
+	
+	private String nom;
+	private Integer force;
+	
+	Romain(String nom, Integer force){
+		this.nom=nom;
+		this.force=force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public String parler(String text) {
+		return "Le romain "+this.nom+": <<"+text+">>";
+	}
+	
+	
 
 }
